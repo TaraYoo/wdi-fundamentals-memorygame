af1049b3b128fea90918a3f90dd0f2d454c95f17
TaraYoo <38822072+qkdbswltn@users.noreply.github.com>
2019-01-21 11:51:23 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,6 +2,25 @@ console.log("test!");
 
 var cards = ['queen','queen','king','king'];
 var cardsInPlay=[];
+
+var checkForMatch = function(){
+  if (cardsInPlay[0]===cardsInPlay[1]){
+    console.log('You found a match!');
+  } else{
+    console.log('Sorry, try again');
+  }
+}
+
+var flipCard = function(cardId){
+  console.log(`User flipped ${cards[cardId]}`)
+  cardsInPlay.push(cards[cardId])
+  if (cardsInPlay.length===2){
+    checkForMatch();
+  }
+}
+
+
+/*
 var cardOne=cards[0];
 
 cardsInPlay.push(cardOne);
@@ -20,3 +39,4 @@ if (cardsInPlay.length===2){
     alert('Sorry, try again');
   }
 }
+*/
